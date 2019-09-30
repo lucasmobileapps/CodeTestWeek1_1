68374b0884e5487bb2790f743b0127db0eae8abb
Lucas Jean Bourgeault <lucas.bourgeault@mobileappscompany.us>
2019-09-30 18:01:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/codetestweek1_1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.codetestweek1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int arr[] = {2, 5, 2, 1, 3, 2, 1};
+        int n = arr.length;
+
+        String frequent = Integer.toString(mostFrequent(arr,n));
+        Log.d("TAG", frequent);
+    }
+    static int mostFrequent(int arr[], int n)
+    {
+
+        // Sort the array
+        Arrays.sort(arr);
+
+        // find the max frequency using linear
+        // traversal
+        int max_count = 1, res = arr[0];
+        int curr_count = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] == arr[i - 1])
+                curr_count++;
+            else
+            {
+                if (curr_count > max_count)
+                {
+                    max_count = curr_count;
+                    res = arr[i - 1];
+                }
+                curr_count = 1;
+            }
+        }
+
+        // If last element is most frequent
+        if (curr_count > max_count)
+        {
+            max_count = curr_count;
+            res = arr[n - 1];
+        }
+
+        return res;
+    }
+}
